f4aa08e57a3083ff5c1b676643fbad075d4aa433
braulioportela79 <braulioportela@outlook.com>
2022-10-08 15:10:45 -0300

    Form Page Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,45 @@
+import * as S from './style';
+
 export const App = () => {
-  return <div>...</div>;
+  return (
+    <S.Container>
+      <S.Main>
+        <S.Form>
+          <S.FormHeader>Reach out to us!</S.FormHeader>
+          <S.TextInput
+            placeholder='Your name*'
+            type='text'
+            required
+          ></S.TextInput>
+          <S.TextInput
+            placeholder='Your e-mail*'
+            type='email'
+            required
+          ></S.TextInput>
+          <S.MsgInput
+            placeholder='Your e-mail*'
+            type='text'
+            required
+          ></S.MsgInput>
+          <S.SubmitBtn>Send message</S.SubmitBtn>
+        </S.Form>
+      </S.Main>
+      <S.Footer>
+        <S.SocialMedia>
+          <a href='/'>
+            <img src='../assets/linkedin_icon.png' alt='' />
+          </a>
+          <a href='/'>
+            <img src='../assets/twitter_icon.png' alt='' />
+          </a>
+          <a href='/'>
+            <img src='../assets/facebook_icon.png' alt='' />
+          </a>
+          <a href='/'>
+            <img src='../assets/pinterest_icon.png' alt='' />
+          </a>
+        </S.SocialMedia>
+      </S.Footer>
+    </S.Container>
+  );
 };
