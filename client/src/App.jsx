74218b4c29b6bb38f4c4d4581eec10a916ef9962
braulioportela79 <braulioportela@outlook.com>
2022-10-09 16:28:08 -0300

    Notification on Message Sent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,53 @@
 import * as S from './style';
 import Feedback from './apis/Feedback';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const notify = () => toast('Your message was sent succesfully!');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await Feedback.post('/', {
-      user_name: userName,
-      user_email: userEmail,
-      message,
-    });
-    setUserName('');
-    setUserEmail('');
-    setMessage('');
+    try {
+      await Feedback.post('/', {
+        user_name: userName,
+        user_email: userEmail,
+        message,
+      });
+      setUserName('');
+      setUserEmail('');
+      setMessage('');
+      notify();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <S.Container>
+      <ToastContainer
+        position='top-center'
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <S.Main>
         <S.Form onSubmit={handleSubmit}>
           <S.FormHeader>Reach out to us!</S.FormHeader>
           <S.TextInput
             placeholder='Your name*'
             type='text'
+            maxLength='100'
             required
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
@@ -34,6 +55,7 @@ export const App = () => {
           <S.TextInput
             placeholder='Your e-mail*'
             type='email'
+            maxLength='100'
             required
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
@@ -41,6 +63,7 @@ export const App = () => {
           <S.MsgInput
             placeholder='Your e-mail*'
             type='text'
+            maxLength='255'
             required
             value={message}
             onChange={(e) => setMessage(e.target.value)}
