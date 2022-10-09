a3e2ea89d952906e3405cf79b872af864da87939
braulioportela79 <braulioportela@outlook.com>
2022-10-09 12:58:15 -0300

    Server Running on Heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const configService = app.get(ConfigService);
-  const port = configService.get('SERVER_PORT');
-  await app.listen(port);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
